5f33deab7a773611ce1aef1dcd316150f670ba95
Wiljay <wiljay.flores@gmail.com>
2018-07-31 12:23:13 -0400

    first commit

---
+++ Maths/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Maths
+//
+//  Created by Wiljay Flores on 2018-07-31.
+//  Copyright © 2018 wiljay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        while(true){
+            
+        }
+    }
+    return 0;
+}
